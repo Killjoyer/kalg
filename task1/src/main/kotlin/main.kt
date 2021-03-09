7d2663f9e4b7d9e64f63f655ae9eb3ac7012a0c4
Simon Zaidelman <saidelsam@ya.ru>
2021-03-09 13:16:23 +0500

    used verts in task 1

--- task1/src/main/kotlin/main.kt
+++ task1/src/main/kotlin/main.kt
@@ -1,19 +1,22 @@
 import java.io.File
 
-internal typealias Labyrinth = MutableList<List<Int>>
+internal typealias Labyrinth<T> = MutableList<MutableList<T>>
 
 fun main(args: Array<String>) {
     val inputFile = File("task1/in.txt").readLines().iterator()
     val outputFile = File("task1/out.txt").printWriter()
     val lines = inputFile.next().toInt()
     val rows = inputFile.next().toInt()
-    val labyrinth: Labyrinth = mutableListOf()
-    for (i in 0 until lines)
+    val labyrinth: Labyrinth<Int> = mutableListOf()
+    val visited: Labyrinth<Boolean> = mutableListOf()
+    (0 until lines).forEach { _ ->
         labyrinth.add(readIntegerList(inputFile))
+        visited.add(MutableList(rows) { false })
+    }
     val startPosition = readIntegerList(inputFile).map { it - 1 }
     val endPosition = readIntegerList(inputFile).map { it - 1 }
 
-    val result = labyrinth.bfs(startPosition, endPosition)
+    val result = labyrinth.bfs(startPosition, endPosition, visited)
     if (result == null) {
         outputFile.use { it.println("N") }
     } else {
@@ -24,27 +27,31 @@ fun main(args: Array<String>) {
     }
 }
 
-fun Labyrinth.bfs(startPosition: List<Int>, endPosition: List<Int>): Path? {
+fun Labyrinth<Int>.bfs(startPosition: List<Int>, endPosition: List<Int>, visited: Labyrinth<Boolean>): Path? {
     var result: Path? = null
     val queue = ArrayDeque<Path>()
-    queue.addLast(Path(null, startPosition))
+    queue.addLast(Path(null, startPosition, startPosition))
     while (!queue.isEmpty()) {
         val currentPath = queue.removeFirst()
         val (curX, curY) = currentPath.value
+        visited[curX][curY] = true
         if (curX == endPosition[0] && curY == endPosition[1]) {
             result = currentPath
             break
         }
-        this.getNeighbours(curX, curY).forEach { queue.addLast(currentPath.addNextStep(it)) }
+        this.getNeighbours(curX, curY)
+            .forEach { if (visited[it[0]][it[1]])
+                queue.addLast(currentPath.addNextStep(it)) }
     }
     return result
 }
 
-fun readIntegerList(file: Iterator<String>): List<Int> = file.next().split(' ').map { it.toInt() }
+fun readIntegerList(file: Iterator<String>): MutableList<Int> = file.next().split(' ').map { it.toInt() }
+    .toMutableList()
 
-fun Labyrinth.isInBounds(x: Int, y: Int): Boolean = x < this.size && y < this[0].size && x >= 0 && y >= 0
+fun Labyrinth<Int>.isInBounds(x: Int, y: Int): Boolean = x < this.size && y < this[0].size && x >= 0 && y >= 0
 
-fun Labyrinth.getNeighbours(x: Int, y: Int): List<List<Int>> {
+fun Labyrinth<Int>.getNeighbours(x: Int, y: Int): List<List<Int>> {
     val result = mutableListOf<List<Int>>()
     Pair(x, y).apply {
         checkNeighbour(0, -1, this@getNeighbours, result)
@@ -58,7 +65,7 @@ fun Labyrinth.getNeighbours(x: Int, y: Int): List<List<Int>> {
 private fun Pair<Int, Int>.checkNeighbour(
     dx: Int,
     dy: Int,
-    labyrinth: Labyrinth,
+    labyrinth: Labyrinth<Int>,
     result: MutableList<List<Int>>
 ) {
     if (labyrinth.isInBounds(this.first + dx, this.second + dy) && labyrinth[this.first + dx][this.second + dy] != 1) {
@@ -66,8 +73,8 @@ private fun Pair<Int, Int>.checkNeighbour(
     }
 }
 
-class Path(private val previous: Path?, val value: List<Int>) : Iterable<Path> {
-    fun addNextStep(child: List<Int>): Path = Path(this, child)
+class Path(private val previous: Path?, val value: List<Int>, val cameFrom: List<Int>) : Iterable<Path> {
+    fun addNextStep(child: List<Int>): Path = Path(this, child, this.value)
 
     override fun iterator(): Iterator<Path> = generateSequence(this) { it.previous }.iterator()
 
